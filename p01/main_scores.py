28b03b521ed8547c9c8ad4771ab24ca5d397883b
Ben Chen <idben.chen@gmail.com>
2026-07-20 09:25:53 +0800

    定義 main_scores 的 function

---
+++ p01/main_scores.py
@@ -0,0 +1,24 @@
+# 找出最高分
+def get_top_student(students_data: list) -> dict:
+    pass
+
+# 算出平均分數
+def get_avg_score(students_data: list) -> float:
+    pass
+
+# 找出分數低於 80 的學生
+def get_lower_score_students(students_data: list, score_limit: int) -> list:
+    pass
+
+
+def main():
+    students = [
+        {"姓名": "小安", "分數": 92},
+        {"姓名": "小美", "分數": 85},
+        {"姓名": "小華", "分數": 76},
+        {"姓名": "小杰", "分數": 60},
+        {"姓名": "小琳", "分數": 98},
+    ]
+
+if __name__ == "__main__":
+    main()
