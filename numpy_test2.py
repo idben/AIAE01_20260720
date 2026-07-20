d7006dae57c163e87e948f2fe87be83b0b19cde7
Ben Chen <idben.chen@gmail.com>
2026-07-20 11:57:23 +0800

    布林遮罩

--- numpy_test2.py
+++ numpy_test2.py
@@ -13,6 +13,13 @@ print(prices3)
 # 讓兩組 array 互相做運算，以索引值對照
 prices = np.array([100, 100, 100, 100])
 # discount = np.array([85, 88, 70, 92, 64]) # 長度不對等時會 ValueError
-discount = np.array([85, 88, 70, 92])
-new_prices = prices * (discount / 100)
+discounts = np.array([85, 88, 70, 92])
+new_prices = prices * (discounts / 100)
 print(new_prices)
+
+# 布林遮罩      # 回傳 True/False 的等長度 array
+# >=80
+is_high_scores = scores >= 80
+print(is_high_scores)
+high_scores = scores[is_high_scores] # 需要再用原始 array 對應出結果
+print(high_scores)
